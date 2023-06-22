f9579dcc4a6c23e472da9493438d2c87592db7e3
MonyDev <rysarakmony1666@gmail.com>
2023-06-22 16:06:10 +0700

    react-ts

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import Nav from './Nav';
+import useCart from '../hooks/useCart';
+
+type PropsType = {
+  viewCart: boolean;
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Header = ({ viewCart, setViewCart }: PropsType) => {
+  const { totalItems, totalPrice } = useCart();
+
+  const content = (
+    <header className="header">
+      <div className="header__title-bar">
+        <h1>Shopping Cart</h1>
+        <div className="header__price-box">
+          <p>Total Items: {totalItems}</p>
+          <p>Total Price: {totalPrice}</p>
+        </div>
+        <Nav viewCart={viewCart} setViewCart={setViewCart} />
+      </div>
+    </header>
+  );
+
+  return content;
+};
+
+export default Header;
